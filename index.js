2038cac6fbda9155e9266dc76a25b8aca5f07986
Anu Siraj <anusiraj007@gmail.com>
2022-11-09 12:39:46 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const e = require('express');
+const express = require('express');
+const app = express(); //app represents this application including a bunch of methods(lie get, put, post, delete)
+
+app.get('/', (req,res) => {
+    res.send("Hello World!. This is Anu Siraj Jencir");
+});
+app.get('/api/courses', (req,res) => {
+    res.send([1,2,3]);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to port ${port}...`));
